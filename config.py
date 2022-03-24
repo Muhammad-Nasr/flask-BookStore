7b7b790ea33c0a0b72a5ec8f69c3d727e9fc446e
Muhammad Nasr <88571996+Muhammad-Nasr@users.noreply.github.com>
2022-03-24 15:21:30 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class BaseConfig(object):
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:123456@localhost/book_store"
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
